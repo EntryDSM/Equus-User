4c66c2c12c628ca717c970688fec4f808199f0c0
HyunSu1768 <azxcv1768@gmail.com>
2023-09-11 07:35:43 +0900

    ✨:: (ENTRY-12) ConfigurationPropertiesScan 활성화

--- src/main/kotlin/hs/kr/equus/user/EquusAuthApplication.kt
+++ src/main/kotlin/hs/kr/equus/user/EquusAuthApplication.kt
@@ -1,8 +1,10 @@
 package hs.kr.equus.user
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class EquusAuthApplication
 
