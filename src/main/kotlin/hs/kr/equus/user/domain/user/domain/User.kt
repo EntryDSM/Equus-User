3575859d855e695b089c22d111d3101be7b57b32
HyunSu1768 <azxcv1768@gmail.com>
2023-09-11 07:35:08 +0900

    ✨:: (ENTRY-12) User Entity 추가

---
+++ src/main/kotlin/hs/kr/equus/user/domain/user/domain/User.kt
@@ -0,0 +1,26 @@
+package hs.kr.equus.user.domain.user.domain
+
+import hs.kr.equus.user.domain.BaseTimeEntity
+import javax.persistence.*
+
+@Entity(name = "tbl_user")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(columnDefinition = "char(11)", nullable = false, unique = true)
+    val telephoneNumber: String,
+
+    @Column(columnDefinition = "char(60)", nullable = false)
+    val password: String,
+
+    @Column(columnDefinition = "char(5)", nullable = false)
+    val name: String,
+
+    @Column(columnDefinition = "bit(1) default 1", nullable = false)
+    val isStudent: Boolean,
+
+    @Column(name = "entry_info_id", nullable = true)
+    val entryInfoId: Long
+) : BaseTimeEntity()
