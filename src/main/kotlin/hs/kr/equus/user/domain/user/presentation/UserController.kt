f967d714cf935eee157208387c4cfe8fdc115793
HyunSu1768 <azxcv1768@gmail.com>
2023-09-21 09:36:13 +0900

    ✨ :: (ENTRY-26) 회원탈퇴 API 개발

--- src/main/kotlin/hs/kr/equus/user/domain/user/presentation/UserController.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/presentation/UserController.kt
@@ -3,10 +3,7 @@ package hs.kr.equus.user.domain.user.presentation
 import hs.kr.equus.user.domain.user.presentation.dto.request.ChangePasswordRequest
 import hs.kr.equus.user.domain.user.presentation.dto.request.UserLoginRequest
 import hs.kr.equus.user.domain.user.presentation.dto.request.UserSignupRequest
-import hs.kr.equus.user.domain.user.service.ChangePasswordService
-import hs.kr.equus.user.domain.user.service.UserLoginService
-import hs.kr.equus.user.domain.user.service.UserSignupService
-import hs.kr.equus.user.domain.user.service.UserTokenRefreshService
+import hs.kr.equus.user.domain.user.service.*
 import hs.kr.equus.user.global.utils.token.dto.TokenResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -18,7 +15,8 @@ class UserController(
     private val userSignupService: UserSignupService,
     private val userLoginService: UserLoginService,
     private val changePasswordService: ChangePasswordService,
-    private val userTokenRefreshService: UserTokenRefreshService
+    private val userTokenRefreshService: UserTokenRefreshService,
+    private val userWithdrawalService: UserWithdrawalService
 ) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -45,4 +43,7 @@ class UserController(
     @PutMapping("/auth")
     fun tokenRefresh(@RequestHeader("X-Refresh-Token") refreshToken: String): TokenResponse =
         userTokenRefreshService.execute(refreshToken)
+
+    @DeleteMapping
+    fun withdrawal() = userWithdrawalService.execute()
 }
---
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/UserWithdrawalService.kt
@@ -0,0 +1,23 @@
+package hs.kr.equus.user.domain.user.service
+
+import hs.kr.equus.user.domain.user.domain.repository.UserRepository
+import hs.kr.equus.user.domain.user.facade.UserFacade
+import hs.kr.equus.user.infrastructure.kafka.configuration.KafkaTopics
+import hs.kr.equus.user.infrastructure.kafka.dto.DeleteUserEventRequest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserWithdrawalService(
+    private val deleteUserKafkaTemplate: KafkaTemplate<String, DeleteUserEventRequest>,
+    private val userFacade: UserFacade,
+    private val userRepository: UserRepository
+) {
+    @Transactional
+    fun execute(){
+        val user = userFacade.getCurrentUser();
+        userRepository.deleteById(user.id)
+        deleteUserKafkaTemplate.send(KafkaTopics.DELETE_USER, DeleteUserEventRequest(user.id))
+    }
+}
