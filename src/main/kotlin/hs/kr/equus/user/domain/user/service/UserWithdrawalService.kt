f63702afd666253d3da39532627258b47a5f5737
HyunSu1768 <azxcv1768@gmail.com>
2023-09-21 17:49:17 +0900

    ♻️ :: (ENTRY-26) 의존성 분리

--- src/main/kotlin/hs/kr/equus/user/domain/user/service/UserWithdrawalService.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/UserWithdrawalService.kt
@@ -2,15 +2,13 @@ package hs.kr.equus.user.domain.user.service
 
 import hs.kr.equus.user.domain.user.domain.repository.UserRepository
 import hs.kr.equus.user.domain.user.facade.UserFacade
-import hs.kr.equus.user.infrastructure.kafka.configuration.KafkaTopics
-import hs.kr.equus.user.infrastructure.kafka.dto.DeleteUserEventRequest
-import org.springframework.kafka.core.KafkaTemplate
+import hs.kr.equus.user.infrastructure.kafka.producer.DeleteUserProducer
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserWithdrawalService(
-    private val deleteUserKafkaTemplate: KafkaTemplate<String, DeleteUserEventRequest>,
+    private val deleteUserProducer: DeleteUserProducer,
     private val userFacade: UserFacade,
     private val userRepository: UserRepository
 ) {
@@ -18,6 +16,6 @@ class UserWithdrawalService(
     fun execute() {
         val user = userFacade.getCurrentUser()
         userRepository.deleteById(user.id)
-        deleteUserKafkaTemplate.send(KafkaTopics.DELETE_USER, DeleteUserEventRequest(user.id))
+        deleteUserProducer.send(user.id)
     }
 }
