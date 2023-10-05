a39de09ce087bd730120a0851a5b84ce08f75431
HyunSu1768 <azxcv1768@gmail.com>
2023-10-05 19:42:40 +0900

    feat :: Add AdminFacade

---
+++ src/main/kotlin/hs/kr/equus/user/domain/admin/facade/AdminFacade.kt
@@ -0,0 +1,21 @@
+package hs.kr.equus.user.domain.admin.facade
+
+import hs.kr.equus.user.domain.admin.domain.Admin
+import hs.kr.equus.user.domain.admin.exception.AdminNotFoundException
+import hs.kr.equus.user.domain.admin.repository.AdminRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class AdminFacade(
+    private val adminRepository: AdminRepository
+) {
+    fun getCurrentUser(): Admin {
+        val adminId = SecurityContextHolder.getContext().authentication.name
+        return adminRepository.findByIdOrNull(adminId) ?: throw AdminNotFoundException
+    }
+
+    fun getUserById(adminId: String): Admin = adminRepository.findByIdOrNull(adminId) ?: throw AdminNotFoundException
+}
