9e4955dc69082e0344223d769d7b7a0a0039f51e
HyunSu1768 <azxcv1768@gmail.com>
2023-10-05 20:36:21 +0900

    refactor :: klint

--- src/main/kotlin/hs/kr/equus/user/domain/admin/domain/Admin.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/admin/domain/Admin.kt
@@ -4,7 +4,6 @@ import hs.kr.equus.user.domain.BaseUUIDEntity
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.Id
 
 @Entity(name = "tbl_admin")
 class Admin(
