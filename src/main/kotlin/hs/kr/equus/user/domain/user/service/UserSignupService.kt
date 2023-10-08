36cc0324142adda59f82fa5bf29ad37c8de9bc94
HyunSu1768 <azxcv1768@gmail.com>
2023-10-08 16:38:47 +0900

    refactor :: klint

--- src/main/kotlin/hs/kr/equus/user/domain/user/service/UserSignupService.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/UserSignupService.kt
@@ -12,7 +12,6 @@ import hs.kr.equus.user.global.utils.token.dto.TokenResponse
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import javax.management.relation.Role
 
 @Service
 class UserSignupService(
