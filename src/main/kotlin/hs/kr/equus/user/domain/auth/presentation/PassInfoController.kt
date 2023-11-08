a5ee2d69e7211aabb86436bd66919cc0035a2a2d
HyunSu1768 <azxcv1768@gmail.com>
2023-11-08 17:36:53 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/user/domain/auth/presentation/PassInfoController.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/auth/presentation/PassInfoController.kt
@@ -5,7 +5,6 @@ import hs.kr.equus.user.domain.auth.presentation.dto.resopnse.QueryPassInfoRespo
 import hs.kr.equus.user.domain.auth.service.PassPopupService
 import hs.kr.equus.user.domain.auth.service.QueryPassInfoService
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
