05f5d8dc5a51832db287bc67ce3085fc25de5402
HyunSu1768 <azxcv1768@gmail.com>
2023-11-08 15:23:31 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/user/domain/user/domain/repository/UserInfoRepository.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/domain/repository/UserInfoRepository.kt
@@ -3,4 +3,4 @@ package hs.kr.equus.user.domain.user.domain.repository
 import hs.kr.equus.user.domain.user.domain.UserInfo
 import org.springframework.data.repository.CrudRepository
 
-interface UserInfoRepository: CrudRepository<UserInfo, String>
+interface UserInfoRepository : CrudRepository<UserInfo, String>
