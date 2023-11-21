8a67e4852e0995ba9c084e9f43126fb77d5bd129
도경 <dokyungkim2926@gmail.com>
2023-11-21 09:39:36 +0900

    feat :: Add Admin token refresh

---
+++ src/main/kotlin/hs/kr/equus/user/domain/admin/service/TokenRefreshService.kt
@@ -0,0 +1,14 @@
+package hs.kr.equus.user.domain.admin.service
+
+import hs.kr.equus.user.global.security.jwt.JwtTokenProvider
+import hs.kr.equus.user.global.utils.token.dto.TokenResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class TokenRefreshService(
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+    @Transactional
+    fun execute(refreshToken: String): TokenResponse = jwtTokenProvider.reIssue(refreshToken)
+}
