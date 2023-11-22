0c10b608ae6244a9c389441f937caf49fd8c1dee
HyunSu1768 <azxcv1768@gmail.com>
2023-11-22 15:11:15 +0900

    refactor :: ktlint

--- src/main/kotlin/hs/kr/equus/user/domain/user/service/UserLoginService.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/UserLoginService.kt
@@ -1,7 +1,6 @@
 package hs.kr.equus.user.domain.user.service
 
 import hs.kr.equus.user.domain.user.domain.UserInfo
-import hs.kr.equus.user.domain.user.domain.UserRole
 import hs.kr.equus.user.domain.user.domain.repository.UserInfoRepository
 import hs.kr.equus.user.domain.user.domain.repository.UserRepository
 import hs.kr.equus.user.domain.user.exception.PasswordNotValidException
