d045fdf6e08c38e34558e9efa3fc64c320728adc
도경 <dokyungkim2926@gmail.com>
2023-11-29 21:59:21 +0900

    style :: line

--- src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaConfig.kt
+++ src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaConfig.kt
@@ -24,7 +24,6 @@ class KafkaConfig(
         return KafkaTemplate(deleteAllTableProducerFactory())
     }
 
-
     @Bean
     fun deleteUserProducerFactory(): DefaultKafkaProducerFactory<String, DeleteUserEventRequest> {
         return DefaultKafkaProducerFactory(producerConfig())
