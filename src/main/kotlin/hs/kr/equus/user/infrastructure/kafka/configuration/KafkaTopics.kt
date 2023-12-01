7a80e0411c655ab2b49cdd6da65889c95c263279
도경 <dokyungkim2926@gmail.com>
2023-12-02 00:31:43 +0900

    style :: ktlint

--- src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaTopics.kt
+++ src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaTopics.kt
@@ -1,6 +1,6 @@
 package hs.kr.equus.user.infrastructure.kafka.configuration
 
 object KafkaTopics {
-        const val DELETE_USER = "delete-user"
-        const val DELETE_ALL_TABLE = "delete-all-table"
+    const val DELETE_USER = "delete-user"
+    const val DELETE_ALL_TABLE = "delete-all-table"
 }
