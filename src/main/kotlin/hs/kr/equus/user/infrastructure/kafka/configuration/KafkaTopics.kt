69141a3f16ccc7f9da6f309a285d9468f9dc047f
도경 <dokyungkim2926@gmail.com>
2023-12-02 00:30:39 +0900

    refactor :: companion object -> object

--- src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaTopics.kt
+++ src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaTopics.kt
@@ -1,8 +1,6 @@
 package hs.kr.equus.user.infrastructure.kafka.configuration
 
-class KafkaTopics {
-    companion object {
+object KafkaTopics {
         const val DELETE_USER = "delete-user"
         const val DELETE_ALL_TABLE = "delete-all-table"
-    }
 }
