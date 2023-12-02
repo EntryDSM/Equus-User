8becbba69f9add07391d3a83cfd2884e76cda258
도경 <dokyungkim2926@gmail.com>
2023-12-02 21:55:23 +0900

    style :: ktlint

--- src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaConsumerConfig.kt
+++ src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/configuration/KafkaConsumerConfig.kt
@@ -1,14 +1,12 @@
 package hs.kr.equus.user.infrastructure.kafka.configuration
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
 
 @EnableKafka
