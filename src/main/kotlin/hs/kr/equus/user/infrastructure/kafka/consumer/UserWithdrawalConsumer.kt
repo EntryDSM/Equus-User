1a6f6d64a2995c210d42f18727a9bab8885071a7
도경 <dokyungkim2926@gmail.com>
2023-12-26 17:05:17 +0900

    refactor ::  change logic

--- src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/consumer/UserWithdrawalConsumer.kt
+++ src/main/kotlin/hs/kr/equus/user/infrastructure/kafka/consumer/UserWithdrawalConsumer.kt
@@ -1,15 +1,14 @@
 package hs.kr.equus.user.infrastructure.kafka.consumer
 
 import hs.kr.equus.user.domain.user.domain.repository.UserRepository
-import hs.kr.equus.user.domain.user.facade.UserFacade
 import hs.kr.equus.user.infrastructure.kafka.configuration.KafkaTopics
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
 
 @Service
 class UserWithdrawalConsumer(
-    val userFacade: UserFacade,
     val userRepository: UserRepository
 ) {
     @KafkaListener(
@@ -18,8 +17,5 @@ class UserWithdrawalConsumer(
         containerFactory = "kafkaListenerContainerFactory"
     )
     @Transactional
-    fun execute() {
-        val user = userFacade.getCurrentUser()
-        userRepository.deleteById(user.id)
-    }
+    fun execute(userId: UUID?) = userRepository.deleteById(userId)
 }
