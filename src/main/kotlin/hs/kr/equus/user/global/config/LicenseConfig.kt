556657b4ae53152d6af1bc95a1e67d9106893f47
HyunSu1768 <azxcv1768@gmail.com>
2024-01-03 08:16:51 +0900

    feat :: Add LicenseConfig

---
+++ src/main/kotlin/hs/kr/equus/user/global/config/LicenseConfig.kt
@@ -0,0 +1,31 @@
+package hs.kr.equus.user.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import java.io.IOException
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.annotation.PostConstruct
+
+@Configuration
+class LicenseConfig(
+    @Value("\${pass.license_file_url}")
+    val licenseFileURl: String
+) {
+    @PostConstruct
+    fun initialize() {
+        try {
+            URL(licenseFileURl).openStream()
+                .use { inputStream ->
+                    Files.copy(inputStream, Paths.get(PATH))
+                }
+        } catch (e: IOException) {
+            print(e.printStackTrace())
+        }
+    }
+
+    companion object {
+        private const val PATH = "equus/license/V61290000000_IDS_01_PROD_AES_license.dat"
+    }
+}
