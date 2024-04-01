6a8f39fd37525685e806229abf233284c5bebda2
도경 <dokyungkim2926@gmail.com>
2024-04-01 09:51:23 +0900

    modify :: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import org.hidetake.gradle.swagger.generator.GenerateSwaggerUI
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.api.tasks.SourceTask
 
 plugins {
     id("org.springframework.boot") version PluginVersions.SPRING_BOOT_VERSION
@@ -10,7 +7,7 @@ plugins {
     kotlin("jvm") version PluginVersions.JVM_VERSION
     kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
     kotlin("plugin.jpa") version PluginVersions.JPA_PLUGIN_VERSION
-    id("com.epages.restdocs-api-spec") version "0.17.1"
+    id("com.epages.restdocs-api-spec") version "0.15.3"
     id("org.hidetake.swagger.generator") version "2.18.2"
 }
 
@@ -32,26 +29,6 @@ repositories {
     maven { url = uri("https://repo.spring.io/milestone") }
     maven { url = uri("https://repo.spring.io/snapshot") }
 }
-
-val swaggerSources by tasks.registering(SourceTask::class) {
-    configure<SourceTask> {
-        inputs.files(file("${project.buildDir}/api-spec/openapi3.yaml"))
-    }
-}
-
-val openapi3 by tasks.registering(GenerateSwaggerUI::class) {
-    configure<GenerateSwaggerUI> {
-        (this as ExtensionAware).extra["servers"] = listOf(
-            mapOf("url" to "http://배포중인 주소"),
-            mapOf("url" to "http://localhost:8080")
-        )
-        (this as ExtensionAware).extra["title"] = "API 문서"
-        (this as ExtensionAware).extra["description"] = "RestDocsWithSwagger Docs"
-        (this as ExtensionAware).extra["version"] = "0.0.1"
-        (this as ExtensionAware).extra["format"] = "yaml"
-    }
-}
-
 
 dependencies {
 
@@ -101,7 +78,7 @@ dependencies {
     implementation(Dependencies.OPEN_API)
 
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("com.epages:restdocs-api-spec-mockmvc:2.18.2")
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.15.3")
     implementation("org.webjars:swagger-ui:4.11.1")
 }
 
@@ -116,31 +93,10 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.withType<GenerateSwaggerUI> {
-    dependsOn(tasks.getByName("generateOpenApi3"))
-
-    doFirst {
-        val swaggerUIFile = file("*/openapi3.yaml")
-
-        val securitySchemesContent = """
-            |  securitySchemes:
-            |    APIKey:
-            |      type: apiKey
-            |      name: Authorization
-            |      in: header
-            |security:
-            |  - APIKey: []  # Apply the security scheme here
-        """.trimMargin()
-
-        swaggerUIFile.appendText(securitySchemesContent)
-    }
-}
-
-
-
-tasks.named<BootJar>("bootJar") {
-    dependsOn("generateSwaggerUISample")
-    from(tasks["generateSwaggerUISample"].outputs.files) {
-        into("static/docs")
-    }
+openapi3 {
+    this.setServer("https://localhost:8080")
+    title = "Equus User API"
+    description = "Equus User API Description"
+    version = "0.1.0"
+    format = "yaml"
 }
