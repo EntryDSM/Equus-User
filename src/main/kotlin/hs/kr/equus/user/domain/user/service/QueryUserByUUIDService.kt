671084065f9786f943adad6d98152662c5412a4e
HyunSu1768 <azxcv1768@gmail.com>
2024-04-13 12:28:37 +0900

    feat :: 유저 정보 조회 기능 개발

--- src/main/kotlin/hs/kr/equus/user/domain/user/service/QueryUserByUUIDService.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/QueryUserByUUIDService.kt
@@ -2,7 +2,7 @@ package hs.kr.equus.user.domain.user.service
 
 import hs.kr.equus.user.domain.user.domain.repository.UserRepository
 import hs.kr.equus.user.domain.user.exception.UserNotFoundException
-import hs.kr.equus.user.domain.user.presentation.dto.response.UserResponse
+import hs.kr.equus.user.domain.user.presentation.dto.response.InternalUserResponse
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,9 +13,9 @@ class QueryUserByUUIDService(
     private val userRepository: UserRepository
 ) {
     @Transactional(readOnly = true)
-    fun execute(userId: UUID): UserResponse {
+    fun execute(userId: UUID): InternalUserResponse {
         val user = userRepository.findByIdOrNull(userId) ?: throw UserNotFoundException
-        return UserResponse(
+        return InternalUserResponse(
             id = user.id!!,
             phoneNumber = user.phoneNumber,
             name = user.name,
