e6e628c0d401c15ada71e6f007808c27a5bb4780
HyunSu1768 <azxcv1768@gmail.com>
2024-06-26 19:36:28 +0900

    bug :: re issue 버그 수정

--- src/main/kotlin/hs/kr/equus/user/domain/user/service/UserTokenRefreshService.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/user/service/UserTokenRefreshService.kt
@@ -1,12 +1,31 @@
 package hs.kr.equus.user.domain.user.service
 
+import hs.kr.equus.user.domain.user.domain.UserInfo
+import hs.kr.equus.user.domain.user.domain.repository.UserInfoRepository
+import hs.kr.equus.user.global.security.jwt.JwtProperties
 import hs.kr.equus.user.global.security.jwt.JwtTokenProvider
 import hs.kr.equus.user.global.utils.token.dto.TokenResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @Service
 class UserTokenRefreshService(
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val jwtProperties: JwtProperties,
+    private val userInfoRepository: UserInfoRepository
 ) {
-    fun execute(refreshToken: String): TokenResponse = jwtTokenProvider.reIssue(refreshToken)
+    fun execute(refreshToken: String): TokenResponse {
+        val tokenResponse = jwtTokenProvider.reIssue(refreshToken)
+
+        val userInfo = UserInfo(
+            token = tokenResponse.accessToken,
+            userId = jwtTokenProvider.getSubjectWithExpiredCheck(tokenResponse.accessToken),
+            userRole = jwtTokenProvider.getRole(tokenResponse.accessToken),
+            ttl = jwtProperties.accessExp
+        )
+
+        userInfoRepository.save(userInfo)
+        return tokenResponse
+    }
 }
