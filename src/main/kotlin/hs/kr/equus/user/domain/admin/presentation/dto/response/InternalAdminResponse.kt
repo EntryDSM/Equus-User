2b13ff8509e69e0555ed9caa51b5cacc0719851e
HyunSu1768 <azxcv1768@gmail.com>
2024-09-11 09:06:27 +0900

    style: format InternalAdminResponse data class for consistency and readability chore(SecurityConfig.kt): remove unused import for CorsUtils to clean up code

--- src/main/kotlin/hs/kr/equus/user/domain/admin/presentation/dto/response/InternalAdminResponse.kt
+++ src/main/kotlin/hs/kr/equus/user/domain/admin/presentation/dto/response/InternalAdminResponse.kt
@@ -2,6 +2,6 @@ package hs.kr.equus.user.domain.admin.presentation.dto.response
 
 import java.util.UUID
 
-data class InternalAdminResponse (
+data class InternalAdminResponse(
     val id: UUID
 )
--- src/main/kotlin/hs/kr/equus/user/global/security/SecurityConfig.kt
+++ src/main/kotlin/hs/kr/equus/user/global/security/SecurityConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.web.cors.CorsUtils
 
 @Configuration
 class SecurityConfig(
